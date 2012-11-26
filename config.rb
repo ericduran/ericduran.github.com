147e9ced566d8641eca5600807d773d487f15bf7
Eric J. Duran <eric.duran7@gmail.com>
2012-11-26 18:05:33 -0500

    Adding basic styles

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
