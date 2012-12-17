679df8f03d4860f7b4478d7fa00cd1f05bc1b676
Eric J. Duran <eric.duran7@gmail.com>
2012-12-17 10:50:41 -0500

    Updating compass config

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
-require 'breakpoint'
+require 'compass-normalize';
+require 'breakpoint';
 
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
